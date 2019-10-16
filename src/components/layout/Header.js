66672d7bd55a6a9673d5ea5baa7da9e887f8b2ae
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-10-16 14:16:34 +0100

    Revert "Merge branch 'master' of https://github.com/adebayoileri/Todo-list-react-app"
    This reverts commit e1628a9a9fcd30b69889558e131c535a3ea02384, reversing
    changes made to f5b3eb0e997f51af8ad9649a0c66bd65c7bd7443.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Header(){
     return(
         <header style={headerStyle}>
             <h1>TodoList App</h1>
-            <Link style={linkStyle} to="/">Home</Link>
-            <Link style={linkStyle} to="/about">About</Link>
         </header>
     )
 }
@@ -15,10 +12,5 @@ const headerStyle ={
     color:'#fff',
     textAlign:'center',
     padding:'10px'
-}
-const linkStyle ={
-    color:'#ffff',
-    fontSize:'24px',
-    margin: '14px'
 }
 export default Header;
