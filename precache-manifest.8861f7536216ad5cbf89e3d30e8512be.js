0ddbe6a4d1189459c6b552715a35605f446ed5e0
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-10-17 10:11:56 +0100

    Updates

---
+++ precache-manifest.8861f7536216ad5cbf89e3d30e8512be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e91aa26c2df1b590479a6c9fceaf015",
+    "url": "/Todo-list-react-app/index.html"
+  },
+  {
+    "revision": "2bde23b34af26fd0a535",
+    "url": "/Todo-list-react-app/static/css/main.4b0463dd.chunk.css"
+  },
+  {
+    "revision": "cf46b27fb7e037222dbb",
+    "url": "/Todo-list-react-app/static/js/2.ba5e77fa.chunk.js"
+  },
+  {
+    "revision": "2bde23b34af26fd0a535",
+    "url": "/Todo-list-react-app/static/js/main.e8e8f16e.chunk.js"
+  },
+  {
+    "revision": "3b4c0572e0bd7bc28ade",
+    "url": "/Todo-list-react-app/static/js/runtime-main.a71e5c70.js"
+  }
+]);
